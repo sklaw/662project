553a72bbde849b8a674dbf0ab205833b6c41e7a1
dxing97 <danielxing97@gmail.com>
2020-12-07 16:08:06 -0500

    implement partially contigious brute force search add randomized A matrix generation to main.py

--- bruteforce.py
+++ bruteforce.py
@@ -2,63 +2,86 @@ import numpy as np
 import random
 import itertools
 from typing import List
+import time
+from tqdm import tqdm
 
-def rotate(x: np.ndarray):
-    for i in range(len(x)):
-        yield np.roll(x, i)
+# def rotate(x: np.ndarray):
+#     for i in range(len(x)):
+#         yield np.roll(x, i)
+
+
+
+def relaxed_contigious_permute(y: float, c: int, n: int):
+    if y == 1:
+        for idxs in itertools.combinations(range(n), c):
+            x = np.zeros(shape=n)
+            x[idxs,] = 1
+            yield x
+    elif y == 0:
+        x = np.zeros(shape=n)
+        x[0:c] = 1
+        for i in range(n-c+1):
+            yield np.roll(x, i)
+    else:
+        for idxs in itertools.combinations(range(n), c):
+            permx = np.zeros(shape=n)
+            permx[idxs,] = 1
+
+            breaks = np.abs((np.correlate(permx, np.array([1, 1])) == 2).sum() - c + 1)
+            if breaks > y * (c-1):
+                continue
+            # print(np.abs((np.correlate(permx, np.array([1, 1])) == 2).sum() - c + 1))
+            yield np.array(permx)
 
 def main():
-    m = 8  # students
-    n = 7  # time slots
-    c = 2  # meeting slots
-    contigious = True
+    m = 100  # students
+    n = 100  # time slots
+    c = 5  # meeting slots
+
+    y = 4 / (c-1)  # y(c-1) = maximum number of breaks
+
+    terminate_early = False
 
     assert c <= n
+    assert y <= 1 and y >= 0
 
-    p = 0.5  # probability of a student being available at any particular time
+    p = 0.7  # probability of a student being available at any particular time
     A = np.random.choice(a=[0, 1], size=(m, n), p=[1-p, p])
-
-    # choose a random x
-    if contigious:
-        x = np.zeros(shape=n)
-        xsel = random.randint(0, n - c)
-        x[xsel:xsel + c] = 1
-    else:
-        xsel = np.random.choice(a=5, size=c, replace=False)
-        x = np.zeros(shape=n)
-        x[xsel] = 1
+    # A = np.array([
+    #     [0, 0, 0, 0, 1, 1, 1, 1],
+    #     [1, 1, 0, 0, 0, 0, 0, 0],
+    #     [1, 0, 1, 1, 0, 0, 0, 0],
+    #     [0, 0, 0, 0, 1, 0, 1, 0],
+    #     [0, 0, 0, 0, 0, 1, 0, 1],
+    #     [0, 1, 0, 0, 0, 1, 0, 1],
+    #     [1, 0, 0, 0, 0, 0, 1, 1],
+    # ])
     print(f"A: {A}")
-    print(f"x: {x}")
-
-    # evaluate objective function
-    obj = (A * x).any(1).sum()
-
-    print(f"obj: {obj}")
-
 
     # iterate over all possible x and choose the best one
     x = np.zeros(shape=n)
     x[0:c] = 1
-    if contigious:
-        iterator = rotate(x)
-    else:
-        iterator = itertools.permutations(x)
+    iterator = relaxed_contigious_permute(y, c, n)
 
     bestobj = 0
     bestx = None
-    for iter, x in enumerate(iterator):
+
+    iter = 0
+    starttime = time.time()
+    for iter, x in tqdm(enumerate(iterator)):
         x = np.array(x)
+        # print(x)
         obj = (A * x).any(1).sum()
         if bestobj < obj:
             bestobj = obj
             bestx = x
-        if bestobj == m:  # early termination
-            print(f"early termination @ evaluation #{iter}")
+        if terminate_early and bestobj == m:  # early termination
+            print(f"early termination @ evaluation #{iter + 1}")
             break
-
+    print(f"Evaluation time: {time.time() - starttime}sec")
     print(f"bestobj: {bestobj}")
     print(f"bestx: {bestx}")
-    print(f"objective function evaluations: {iter}")
+    print(f"objective function evaluations: {iter + 1}")
 
 if __name__ == '__main__':
     main()
--- main.py
+++ main.py
@@ -1,20 +1,32 @@
 import cvxpy as cp
 import numpy as np
+import time
 
 
 if __name__ == "__main__":
-    A = np.array([
-        [0, 0, 0, 0, 1, 1, 1, 1],
-        [1, 1, 0, 0, 0, 0, 0, 0],
-        [1, 0, 1, 1, 0, 0, 0, 0],
-        [0, 0, 0, 0, 1, 0, 1, 0],
-        [0, 0, 0, 0, 0, 1, 0, 1],
-        [0, 1, 0, 0, 0, 1, 0, 1],
-        [1, 0, 0, 0, 0, 0, 1, 1],
-    ])
-    c = 2
+    # A = np.array([
+    #     [0, 0, 0, 0, 1, 1, 1, 1],
+    #     [1, 1, 0, 0, 0, 0, 0, 0],
+    #     [1, 0, 1, 1, 0, 0, 0, 0],
+    #     [0, 0, 0, 0, 1, 0, 1, 0],
+    #     [0, 0, 0, 0, 0, 1, 0, 1],
+    #     [0, 1, 0, 0, 0, 1, 0, 1],
+    #     [1, 0, 0, 0, 0, 0, 1, 1],
+    # ])
+    # c = 2
 
-    m, n = A.shape
+    # m, n = A.shape
+
+    m = 100  # students
+    n = 100  # time slots
+    c = 5  # meeting slots
+
+    assert c <= n
+
+    p = 0.7  # probability of a student being available at any particular time
+    A = np.random.choice(a=[0, 1], size=(m, n), p=[1-p, p])
+
+
     x = cp.Variable(n, boolean=True)
     Y = cp.Variable([m, n])
     Z = cp.Variable([m, n])
@@ -41,26 +53,27 @@ if __name__ == "__main__":
 
     constraints.append(np.ones(n) @ x == c)
 
-    # Contiguous constraint
-    y_1 = cp.Variable(n-1)
-    y_2 = cp.Variable(n-1)
-    y_3 = cp.Variable(n-1)
-    for i in range(n-1):
-        constraints.append(y_1[i] == x[i])
-        constraints.append(y_2[i] == x[i+1])
-        # y_3 = y_1 AND y_2
-        constraints.append(y_3[i] >= y_1[i] + y_2[i] - 1)
-        constraints.append(y_3[i] <= y_1[i])
-        constraints.append(y_3[i] <= y_2[i])
-        constraints.append(y_3[i] <= 1)
-        constraints.append(y_3[i] >= 0)
-    constraints.append(np.ones(n-1)@y_3 == np.ones(n)@x-1)
+    # # Contiguous constraint
+    # y_1 = cp.Variable(n-1)
+    # y_2 = cp.Variable(n-1)
+    # y_3 = cp.Variable(n-1)
+    # for i in range(n-1):
+    #     constraints.append(y_1[i] == x[i])
+    #     constraints.append(y_2[i] == x[i+1])
+    #     # y_3 = y_1 AND y_2
+    #     constraints.append(y_3[i] >= y_1[i] + y_2[i] - 1)
+    #     constraints.append(y_3[i] <= y_1[i])
+    #     constraints.append(y_3[i] <= y_2[i])
+    #     constraints.append(y_3[i] <= 1)
+    #     constraints.append(y_3[i] >= 0)
+    # constraints.append(np.ones(n-1)@y_3 == np.ones(n)@x-1)
 
 
     prob = cp.Problem(cp.Maximize(np.ones(m) @ Z[:,n-1]), constraints)
-
+    starttime = time.time()
     prob.solve()
 
+    print("eval_sec=", time.time() - starttime)
     print("prob.value=", prob.value)
     print("x.value=", x.value)
     print("Y.value=", Y.value)
